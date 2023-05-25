2e9db059f7471dc70b5deb0ac51be5e25b997246
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-05-25 07:34:14 +0300

    varying

--- src/shaders/moving/fragmentShader.js
+++ src/shaders/moving/fragmentShader.js
@@ -1,12 +1,11 @@
 const fragmentShader = `
 varying vec2 vUv;
 
-vec3 colorA = vec3(0.008,0.895,0.940);
-vec3 colorB = vec3(0.129,0.299,1.000);
+vec3 colorA = vec3(0.912,0.191,0.652);
+vec3 colorB = vec3(1.000,0.777,0.052);
 
 void main() {
-  vec2 normalizedPixel = gl_FragCoord.xy/500.0;
-  vec3 color = mix(colorA, colorB, normalizedPixel.x);
+  vec3 color = mix(colorA, colorB, vUv.x);
 
   gl_FragColor = vec4(color,1.0);
 }
--- src/shaders/moving/vertexShader.js
+++ src/shaders/moving/vertexShader.js
@@ -3,6 +3,7 @@ uniform float u_time;
 varying vec2 vUv;
 
 void main() {
+  vUv = uv;
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   modelPosition.y += sin(modelPosition.x * 4.0 + u_time * 2.0) * 0.2;
   modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
